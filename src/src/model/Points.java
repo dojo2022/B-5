4ba6a1ae28c2a8f76d49891db8f2aa55eeccac22
dojo2022-hashima <hashima-yuka-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-14 17:45:07 +0900

    Point ToDoListのmodel

--- src/src/model/Points.java
+++ src/src/model/Points.java
@@ -1,5 +1,47 @@
 package model;
+import java.io.Serializable;
 
-public class Points {
+public class Points implements Serializable{
+	private String id;
+	private String user_id;
+	private String point_value;
 
+	public Points(String id, String user_id, String point_value) {
+		super();
+		this.id = id;
+		this.user_id = user_id;
+		this.point_value = point_value;
+	}
+	//引数がないコンストラクタ（デフォルトコンストラクタ）
+	public Points() {
+		this.id = "";
+		this.user_id = "";
+		this.point_value ="";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setPw(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getPoint_value() {
+		return point_value;
+	}
+
+	public void setPoint_value(String point_value) {
+		this.point_value= point_value;
+	}
 }
+
+
--- src/src/model/TodoLists.java
+++ src/src/model/TodoLists.java
@@ -1,5 +1,71 @@
 package model;
+import java.io.Serializable;
 
-public class TodoLists {
+public class TodoLists implements Serializable{
+	private Int id;
+	private String todo_deadline;
+	private String task;
+	private Int importance;
+	private String todo_memo;
+	private String user_id;
+
+	public TodoLists(String id, String todo_deadline, String task, String importance, String todo_memo,
+			String user_id) {
+		super();
+		this.id = id;
+		this.todo_deadline = todo_deadline;
+		this.task = task;
+		this.importance = importance;
+		this.todo_memo = todo_memo;
+		this.user_id = user_id;
+	}
+
+	public TodoLists() {
+		this.id = "";
+		this.todo_deadline = "";
+		this.task = "";
+		this.importance = "";
+		this.todo_memo= "";
+		this.user_id = "";
+	}
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTodo_deadline() {
+		return todo_deadline;
+	}
+	public void setTodo_deadline(String todo_deadline) {
+		this.todo_deadline = todo_deadline;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public String getImportance() {
+		return importance;
+	}
+	public void setImportance(String importance) {
+		this.importance = importance;
+	}
+	public String getTodo_memo() {
+		return todo_memo;
+	}
+	public void setTodo_memo(String todo_memo) {
+		this.todo_memo = todo_memo;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
 
 }
