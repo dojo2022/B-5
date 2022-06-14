a29f52b28d654cb071cd18aa1437662f4154df7a
dojo2022-Shimizu <103237417+dojo2022-Shimizu@users.noreply.github.com>
2022-06-14 14:52:21 +0900

    model変更　item_change.html変更

--- src/src/model/Characters.java
+++ src/src/model/Characters.java
@@ -1,5 +1,95 @@
 package model;
 
-public class Characters {
+import java.io.Serializable;
+
+public class Characters implements Serializable{
+
+		private String id;
+		private String user_id;
+		private String character_name;
+		private String character_status;
+		private String character_image;
+		private String character_point;
+		private String character_id;
+
+		public Characters(String id, String user_id, String character_name, String character_status,
+				String character_image, String character_point, String character_id) {
+			super();
+			this.id = id;
+			this.user_id = user_id;
+			this.character_name = character_name;
+			this.character_status = character_status;
+			this.character_image = character_image;
+			this.character_point = character_point;
+			this.character_id = character_id;
+		}
+
+		public Characters() {
+			super();
+			this.id = "";
+			this.user_id = "";
+			this.character_name = "";
+			this.character_status = "";
+			this.character_image = "";
+			this.character_point = "";
+			this.character_id = "";
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getUser_id() {
+			return user_id;
+		}
+
+		public void setUser_id(String user_id) {
+			this.user_id = user_id;
+		}
+
+		public String getCharacter_name() {
+			return character_name;
+		}
+
+		public void setCharacter_name(String character_name) {
+			this.character_name = character_name;
+		}
+
+		public String getCharacter_status() {
+			return character_status;
+		}
+
+		public void setCharacter_status(String character_status) {
+			this.character_status = character_status;
+		}
+
+		public String getCharacter_image() {
+			return character_image;
+		}
+
+		public void setCharacter_image(String character_image) {
+			this.character_image = character_image;
+		}
+
+		public String getCharacter_point() {
+			return character_point;
+		}
+
+		public void setCharacter_point(String character_point) {
+			this.character_point = character_point;
+		}
+
+		public String getCharacter_id() {
+			return character_id;
+		}
+
+		public void setCharacter_id(String character_id) {
+			this.character_id = character_id;
+		}
+
 
 }
--- src/src/model/Kakugens.java
+++ src/src/model/Kakugens.java
@@ -1,5 +1,81 @@
 package model;
 
-public class Kakugens {
+import java.io.Serializable;
+
+public class Kakugens implements Serializable{
+	private String id;
+	private String kakugen;
+	private String genre_name;
+	private String genre;
+	private String kakugen_id;
+	private String kakugen_point;
+
+	public Kakugens(String id, String kakugen, String kakugen_name, String genre_name, String genre,
+			String kakugen_id, String kakugen_point) {
+		this.id = id;
+		this.kakugen= kakugen;
+		this.genre_name= genre_name;
+		this.genre = genre;
+		this.kakugen_id= kakugen_id;
+		this.kakugen_point= kakugen_point;
+
+	}
+
+	public Kakugens() {
+		this.id = "";
+		this.kakugen= "";
+		this.genre_name= "";
+		this.genre = "";
+		this.kakugen_id= "";
+		this.kakugen_point= "";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getKakugen() {
+		return kakugen;
+	}
+
+	public void setKakugen(String kakugen) {
+		this.kakugen = kakugen;
+	}
+
+	public String getGenre_name() {
+		return genre_name;
+	}
+
+	public void setGenre_name(String genre_name) {
+		this.genre_name = genre_name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getKakugen_id() {
+		return kakugen_id;
+	}
+
+	public void setKakugen_id(String kakugen_id) {
+		this.kakugen_id = kakugen_id;
+	}
+
+	public String getKakugen_point() {
+		return kakugen_point;
+	}
+
+	public void setKakugen_point(String kakugen_point) {
+		this.kakugen_point = kakugen_point;
+	}
 
 }
